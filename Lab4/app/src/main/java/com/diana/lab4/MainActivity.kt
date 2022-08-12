2758356d7a237ac7129622c6d58b599d8869ca28
FernandezDL <fer21747@uvg.edu.gt>
2022-08-12 09:39:40 -0600

    subida archivos lab 4

---
+++ Lab4/app/src/main/java/com/diana/lab4/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.diana.lab4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var emergenteBoton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        emergenteBoton= findViewById(R.id.primerBoton)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        emergenteBoton.setOnClickListener {
+            Toast.makeText(applicationContext, "Diana Fernández", Toast.LENGTH_LONG).show()
+        }
+    }
+}
